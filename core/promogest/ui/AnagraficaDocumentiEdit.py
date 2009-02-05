6a708c59eb7a3e4e0ae9d58fb1d8bc72e23b0dd3
vete <vete@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2009-02-05 16:47:05 +0000

    fix2

--- core/promogest/ui/AnagraficaDocumentiEdit.py
+++ core/promogest/ui/AnagraficaDocumentiEdit.py
@@ -1300,8 +1300,8 @@ del documento.
             return
 
         self._righe[0]["idMagazzino"] = findIdFromCombobox(self.id_magazzino_combobox)
-        #magazzino = leggiMagazzino(self._righe[0]["idMagazzino"])
-        magazzino = Magazzino().getRecord(id=self._righe[0]["idMagazzino"])
+        magazzino = leggiMagazzino(self._righe[0]["idMagazzino"])
+        #magazzino = Magazzino().getRecord(id=self._righe[0]["idMagazzino"])
         self._righe[0]["magazzino"] = magazzino['denominazione']
 
         if (self.data_documento_entry.get_text() == ''):
