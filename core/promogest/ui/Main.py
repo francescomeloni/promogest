429e7377eb62bf718430f5e3b2c88bccd02d30aa
vete <vete@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2009-09-05 09:42:57 +0000

    ok aggiunta immagini

--- core/promogest/ui/Main.py
+++ core/promogest/ui/Main.py
@@ -34,13 +34,7 @@ class Main(GladeWidget):
         self.main_window.set_title('*** PromoGest2 *** Azienda : '+aziendaStr+'  *** Utente : '+Environment.params['usernameLoggedList'][1]+' ***')
         self.aziendaStr = aziendaStr
 
-        textStatusBar = "    PromoGest2 - 800 034561 - www.promotux.it - info@promotux.it     "
-        context_id =  self.pg2_statusbar.get_context_id("main_window")
-        self.pg2_statusbar.push(context_id,textStatusBar)
-        if Environment.rev_locale < Environment.rev_remota:
-            self.active_img.set_from_file("gui/active_off.png")
-        else:
-            self.active_img.set_from_file("gui/active_on.png")
+        self.statusBarHandler()
 
 
         Login.windowGroup.append(self.getTopLevel())
@@ -162,6 +156,17 @@ class Main(GladeWidget):
         else:
             self.main_hbox.pack_start(self.currentFrame, fill=True, expand=True)
         self.main_hbox.show_all()
+
+
+    def statusBarHandler(self):
+        textStatusBar = "    PromoGest2 - 800 034561 - www.promotux.it - info@promotux.it     "
+        context_id =  self.pg2_statusbar.get_context_id("main_window")
+        self.pg2_statusbar.push(context_id,textStatusBar)
+
+        if Environment.rev_locale < Environment.rev_remota:
+            self.active_img.set_from_file("gui/active_off.png")
+        else:
+            self.active_img.set_from_file("gui/active_on.png")
 
     def on_disconnect(self, widget=None):
         dialog = gtk.MessageDialog(self.getTopLevel(),
--- installa.sh
+++ installa.sh
@@ -1,3 +1,3 @@
 cp core/desktop/gnome/promogest.desktop /usr/share/applications/
 cp core/gui/promogest.png /usr/share/icons/
-cp core/desktop/promoGest2	/usr/bin/
+cp core/desktop/promoGest2 /usr/bin/
