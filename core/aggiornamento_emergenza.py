b10b159f3f34296eda608f634a0bf1e4c25a110f
vete <vete@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2010-05-31 17:34:49 +0000

    aggiunti due mini script che permettono di aggiornare l'svn con facilità

---
+++ core/aggiornamento_emergenza.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Promogest
+#
+# Copyright (C) 2005-2008 by Promotux Informatica - http://www.promotux.it/
+# Author: Francesco Meloni <francesco@promotux.it>
+
+import pysvn
+
+client = pysvn.Client()
+client.exception_style = 0
+client.update( '.' )
