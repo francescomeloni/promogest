bd1ef84c7397fe289f108b8321a410faf9798832
vete <vete@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2010-11-22 16:36:02 +0000

    tfix on ordinamento articoli in ricerca complessa

--- core/promogest/ui/RicercaComplessaArticoli.py
+++ core/promogest/ui/RicercaComplessaArticoli.py
@@ -151,7 +151,7 @@ class RicercaComplessaArticoli(RicercaComplessa):
         column = gtk.TreeViewColumn('Codice articolo fornitore', renderer, text=6, background=1)
         column.set_sizing(gtk.TREE_VIEW_COLUMN_GROW_ONLY)
         column.set_clickable(True)
-        column.connect("clicked", self.filter._changeOrderBy, (None, Fornitura.codice_articolo_fornitore))
+        column.connect("clicked", self.filter._changeOrderBy, (Fornitura, Fornitura.codice_articolo_fornitore))
         column.set_resizable(True)
         column.set_expand(False)
         treeview.append_column(column)
