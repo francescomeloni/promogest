358f1a06ba60437dc0692236e3f885dab7846b4d
vete <vete@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2010-12-17 11:15:17 +0000

    messo un try nel controllo di avvio

--- core/promogest/dao/Dao.py
+++ core/promogest/dao/Dao.py
@@ -96,7 +96,6 @@ class Dao(object):
         self.record= self._session.query(self._DaoModule)
         if sqlalchemy.__version__ > "0.6":
             if join is not None:
-                print "JOIIIIIIIIIIIIIIIIIIIIIIIIIIIIII", join
                 self.record = self.record.join(join)
             if filter is not None:
                 self.record = self.record.filter(filter)
