673d315135d0b8fa72a48f9e3607d5d8fd5ed75a
vete <vete@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2011-01-23 15:55:26 +0000

    pycanberra e rimosso _ in pvcode

--- core/promogest/dao/Magazzino.py
+++ core/promogest/dao/Magazzino.py
@@ -37,9 +37,9 @@ class Magazzino(Dao):
 
 magazzino=Table('magazzino',params['metadata'],schema = params['schema'],autoload=True)
 
-if "pvcode_" not in [c.name for c in magazzino.columns]:
-    col = Column('pvcode_', String)
-    col.create(magazzino)
+#if "pvcode_" not in [c.name for c in magazzino.columns]:
+#    col = Column('pvcode_', String)
+#    col.create(magazzino)
 
 if "pvcode" not in [c.name for c in magazzino.columns]:
     col = Column('pvcode', String)
