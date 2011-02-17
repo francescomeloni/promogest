9754f93a2c329df7bbb0535f223cc5ec74c433f9
francesco <francesco@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2011-02-17 00:40:05 +0000

    committato per errore due print di infopeso

--- core/promogest/ui/AnagraficaClientiEdit.py
+++ core/promogest/ui/AnagraficaClientiEdit.py
@@ -238,8 +238,8 @@ class AnagraficaClientiEdit(AnagraficaEdit):
 
     def saveDao(self):
         self.verificaListino()
-        print "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP", self.dao_tinfopeso
-        print "QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ", self.infopeso_page.citta_nascita_entry.get_text()
+#        print "PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP", self.dao_tinfopeso
+#        print "QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ", self.infopeso_page.citta_nascita_entry.get_text()
         self.dao.codice = self.codice_entry.get_text()
         self.dao.codice = omogeneousCode(section="Clienti", string=self.dao.codice )
         self.dao.ragione_sociale = self.ragione_sociale_entry.get_text()
