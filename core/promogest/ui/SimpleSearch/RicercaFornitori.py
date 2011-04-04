deb6ae4f2b1c5934273d3eca6730bb7bb699259c
vete <vete@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2011-04-04 21:33:23 +0000

    fix in import anag fornitori

--- core/promogest/ui/SimpleSearch/RicercaFornitori.py
+++ core/promogest/ui/SimpleSearch/RicercaFornitori.py
@@ -44,7 +44,7 @@ class RicercaFornitori(Ricerca):
             self.filter.refresh()
             self.filter.ragione_sociale_filter_entry.grab_focus()
 
-        from AnagraficaFornitori import AnagraficaFornitori
+        from promogest.ui.AnagraficaFornitori import AnagraficaFornitori
         anag = AnagraficaFornitori()
         anagWindow = anag.getTopLevel()
 
