917fa92ab247dc7b114bd7efd5258ec61e5e074b
vete <vete@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2011-05-10 15:16:31 +0000

    small fix on stampa euro e mNLC in stampa

--- core/promogest/lib/sla2pdf/SlaTpl2Sla.py
+++ core/promogest/lib/sla2pdf/SlaTpl2Sla.py
@@ -437,6 +437,8 @@ class SlaTpl2Sla(SlaParser):
                                     resolvedTag = str(value)
                                 ch = ch.replace(tags[tagkey]['completeTag'], resolvedTag)
                                   # Save itext
+                                if ch.count('€') >1:
+                                    ch = ch.replace('€', '', 1)
                                 itext.set('CH', ch.decode())
 
                 else:
