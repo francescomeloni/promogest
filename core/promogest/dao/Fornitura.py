bab658007afa05324963dd2807b0d2db09a726b4
francesco <francesco@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2011-05-30 22:54:59 +0000

    fornitura clean

--- core/promogest/dao/Fornitura.py
+++ core/promogest/dao/Fornitura.py
@@ -45,53 +45,42 @@ class Fornitura(Dao):
 
     def _fornitore(self):
         if self.forni: return self.forni.ragione_sociale or ""
-        #else: return ""
     fornitore= property(_fornitore)
 
     def _codice_fornitore(self):
         if self.forni: return self.forni.codice or ""
-        #else: return ""
     codice_fornitore= property(_codice_fornitore)
 
     def _codiceArticolo(self):
         if self.arti: return self.arti.codice or ""
-        #else: return ""
     codice_articolo= property(_codiceArticolo)
 
     def _denoArticolo(self):
         if self.arti: return self.arti.denominazione or ""
-        #else: return ""
     articolo= property(_denoArticolo)
 
     def _multiplo(self):
         if self.multi: return self.multi.denominazione_breve or ""
-        #else: return ""
     multiplo= property(_multiplo)
 
 
     if hasattr(conf, "PromoWear") and getattr(conf.PromoWear,'mod_enable')=="yes":
         def _denominazione_gruppo_taglia(self):
-            #if self.ATC: return self.ATC.denominazione or ""
             if self.arti:return self.arti.denominazione_gruppo_taglia
-            #else: return ""
         denominazione_gruppo_taglia = property(_denominazione_gruppo_taglia)
 
         def _id_articolo_padre(self):
-            #if self.ATC: return self.ATC.id_articolo_padre or None
             if self.arti:return self.arti.id_articolo_padre
         id_articolo_padre_taglia_colore=property(_id_articolo_padre)
         id_articolo_padre = property(_id_articolo_padre)
 
         def _id_gruppo_taglia(self):
-            #if self.ATC: return self.ATC.id_gruppo_taglia or None
             if self.arti:return self.arti.id_gruppo_taglia
         id_gruppo_taglia=property(_id_gruppo_taglia)
 
         def _id_genere(self):
-            #if self.ATC: return self.ATC.id_genere or None
             if self.arti:return self.arti.id_genere
-            #else: return ""
-        id_genere = property(_id_genere)
+            id_genere = property(_id_genere)
 
         def _id_stagione(self):
             if self.arti:return self.arti.id_stagione
@@ -102,27 +91,22 @@ class Fornitura(Dao):
         id_anno = property(_id_anno)
 
         def _denominazione_taglia(self):
-            """ esempio di funzione  unita alla property """
             if self.arti:return self.arti.denominazione_taglia
         denominazione_taglia = property(_denominazione_taglia)
 
         def _denominazione_colore(self):
-            """ esempio di funzione  unita alla property """
             if self.arti:return self.arti.denominazione_colore
         denominazione_colore = property(_denominazione_colore)
 
         def _anno(self):
-            """ esempio di funzione  unita alla property """
             if self.arti:return self.arti.anno
         anno = property(_anno)
 
         def _stagione(self):
-            """ esempio di funzione  unita alla property """
             if self.arti:return self.arti.stagione
         stagione = property(_stagione)
 
         def _genere(self):
-            """ esempio di funzione  unita alla property """
             if self.arti:return self.arti.genere
         genere = property(_genere)
 
