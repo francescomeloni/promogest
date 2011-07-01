5df2a58982cbab8ee119a3a3ed1ab7c6b8f011cb
vete <vete@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2011-07-01 22:43:10 +0000

    un po' di clean e qualche licenza

--- core/promogest/modules/GestioneImmagini/ui/GestioneChiusuraFiscale.py
+++ core/promogest/modules/GestioneImmagini/ui/GestioneChiusuraFiscale.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
 
-# Promogest
-#
-# Copyright (C) 2005-2008 by Promotux Informatica - http://www.promotux.it/
-# Author: Francesco Meloni <francescoo@promotux.it>
+#    Copyright (C) 2005, 2006, 2007 2008, 2009, 2010, 2011 by Promotux
+#                       di Francesco Meloni snc - http://www.promotux.it/
 
-import gtk, gobject
+#    Author: Francesco Meloni  <francesco@promotux.it>
+#    This file is part of Promogest.
+
+#    Promogest is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 2 of the License, or
+#    (at your option) any later version.
+
+#    Promogest is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with Promogest.  If not, see <http://www.gnu.org/licenses/>.
+
+import gtk
 import os, popen2
-import gtkhtml2
-from promogest.dao.DaoUtils import giacenzaSel
 from datetime import datetime, timedelta
 from promogest import Environment
 from promogest.ui.GladeWidget import GladeWidget
