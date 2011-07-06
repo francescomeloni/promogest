4a74873d82876d7fef72b57a33407d3a497bbddd
fmarl <fmarl@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2011-07-06 08:55:52 +0000

    Distingue tra glade 3.8 e 3.10 per i cataloghi glade personalizzati

---
+++ core/glade38/pythonplugin.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#    Copyright (C) 2005, 2006, 2007 2008, 2009, 2010 by Promotux
+#                        di Francesco Meloni snc - http://www.promotux.it/
+
+#    Author: Francesco Meloni  <francesco@promotux.it>
+
+#    This file is part of Promogest.
+
+#    Promogest is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 2 of the License, or
+#    (at your option) any later version.
+
+#    Promogest is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with Promogest.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys, os
+
+path = os.path.split(os.path.dirname(__file__))[0]
+core_path = os.path.join(os.path.split(os.path.abspath(os.path.split(os.path.dirname(__file__))[0]))[0], "core")
+sys.path.append(core_path)
+from promogest import Environment
+Environment.conf.guiDir = path+"/core/gui/"
+#Environment.conf.decimals = 2
+from promogest.ui.GladeWidget import *
+from promogest.ui.widgets import PersonaGiuridicaSearchWidget, FornitoreSearchWidget,\
+                    ClienteSearchWidget,ArticoloSearchWidget,CustomComboBoxModify,\
+                    CustomComboBoxSearch,ScontiWidget,ScontoWidget
