99c95fe3ecfbe7257fb58dcf8092d3dafc3f70a5
fmarl <fmarl@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2011-09-26 11:51:03 +0000

    AnagraficaDocumenti: corretto errore di approssimazione nei calcoli
    http://code.google.com/p/promogest/issues/detail?id=153

--- core/promogest/ui/anagDocumenti/AnagraficaDocumentiEditUtils.py
+++ core/promogest/ui/anagDocumenti/AnagraficaDocumentiEditUtils.py
@@ -341,18 +341,18 @@ def calcolaTotalePart(anaedit, dao=None):
             totaleImponibileScontato += Decimal(castellettoIva[k]['imponibile'])
             totaleImpostaScontata += Decimal(castellettoIva[k]['imposta'])
 
-        totaleScontato = mN(totaleImponibileScontato,2) + mN(totaleImpostaScontata,2)
-    anaedit.totale_generale_label.set_text(str(mN(totaleImponibile,2) + mN(totaleImposta,2)+mN(totaleEsclusoBaseImponibile,2) ))
-    anaedit.totale_generale_riepiloghi_label.set_text(str(mN(totaleImponibile,2) + mN(totaleImposta,2)+mN(totaleEsclusoBaseImponibile,2)))
+        totaleScontato = totaleImponibileScontato + totaleImpostaScontata
+    anaedit.totale_generale_label.set_text(str(mN(totaleImponibile + totaleImposta + totaleEsclusoBaseImponibile, 2)))
+    anaedit.totale_generale_riepiloghi_label.set_text(str(mN(totaleImponibile + totaleImposta + totaleEsclusoBaseImponibile, 2)))
     anaedit.totale_imponibile_label.set_text(str(mN(totaleImponibileScontato, 2)))
-    anaedit.totale_imponibile_riepiloghi_label.set_text(str(mN(totaleImponibile,2)))
+    anaedit.totale_imponibile_riepiloghi_label.set_text(str(mN(totaleImponibile, 2)))
     anaedit.totale_imposta_label.set_text(str(mN(totaleImpostaScontata, 2)))
     anaedit.totale_imposta_riepiloghi_label.set_text(str(mN(totaleImposta, 2)))
     anaedit.totale_imponibile_scontato_riepiloghi_label.set_text(str(mN(totaleImponibileScontato, 2)))
     anaedit.totale_imposta_scontata_riepiloghi_label.set_text(str(mN(totaleImpostaScontata, 2)))
-    anaedit.totale_scontato_riepiloghi_label.set_text(str(mN(totaleImponibileScontato,2) + mN(totaleImpostaScontata,2)))
-    anaedit.pagamenti_page.totale_in_pagamenti_label.set_markup('<b><span foreground="black" size="24000">'+str(mN(totaleScontato,2))+'</span></b>')
-    anaedit.totale_non_base_imponibile_label.set_text(str(mN(totaleEsclusoBaseImponibile,2)))
+    anaedit.totale_scontato_riepiloghi_label.set_text(str(mN(totaleImponibileScontato + totaleImpostaScontata, 2)))
+    anaedit.pagamenti_page.totale_in_pagamenti_label.set_markup('<b><span foreground="black" size="24000">'+str(mN(totaleScontato, 2))+'</span></b>')
+    anaedit.totale_non_base_imponibile_label.set_text(str(mN(totaleEsclusoBaseImponibile, 2)))
 
     id_pag = anaedit._id_pagamento
     pago = Pagamento().getRecord(id=id_pag)
