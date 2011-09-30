e843e34c09e609bc4de8dd1ed8ed39c9494495cc
fmarl <fmarl@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2011-09-30 19:12:59 +0000

    TestataDocumentoScadenza: fix note_per_primanota nullable

--- core/promogest/modules/Pagamenti/dao/TestataDocumentoScadenza.py
+++ core/promogest/modules/Pagamenti/dao/TestataDocumentoScadenza.py
@@ -49,8 +49,10 @@ if "id_banca" not in [c.name for c in tesdocsca.columns]:
     col.create(tesdocsca)
 
 if "note_per_primanota" not in [c.name for c in tesdocsca.columns]:
-    col = Column('note_per_primanota', String(400))
+    col = Column('note_per_primanota', Text, nullable=True)
     col.create(tesdocsca)
+else:
+    tesdocsca.c.note_per_primanota.alter(Text, nullable=True)
 
 std_mapper = mapper(TestataDocumentoScadenza, tesdocsca, properties={},
                                 order_by=tesdocsca.c.id)
