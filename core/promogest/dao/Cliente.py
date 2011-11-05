ecc0763c0e415061113f10786195555fd4745a0e
fmarl <fmarl@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2011-11-05 12:24:19 +0000

    Cliente: aggiunge id_aliquota_iva alla tabella

--- core/promogest/dao/Cliente.py
+++ core/promogest/dao/Cliente.py
@@ -178,6 +178,10 @@ cliente = Table('cliente',
               schema=params['schema'],
               autoload=True)
 
+if 'id_aliquota_iva' not in [c.name for c in cliente.columns]:
+    col = Column('id_aliquota_iva', Integer, nullable=True)
+    col.create(cliente, populate_default=True)
+
 j = join(cliente, persona_giuridica)
 
 std_mapper = mapper(Cliente,j, properties={
