bd6503e1e12fe6bdf0db1fdc4fa4e85adc0cbc88
francesco <francesco@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2011-11-15 15:32:36 +0000

    fix cose cosi

--- core/promogest/dao/TestataDocumento.py
+++ core/promogest/dao/TestataDocumento.py
@@ -308,10 +308,10 @@ class TestataDocumento(Dao):
                 if riga.id_articolo and riga.id_listino:
                     from promogest.ui.utils import leggiListino
                     ll = leggiListino(riga.id_listino, riga.id_articolo)
-                    totaleRicaricatoLordo += (Decimal(riga.valore_unitario_netto or 0) - ll["ultimoCosto"])
+                    totaleRicaricatoLordo += (Decimal(riga.quantita or 0) * Decimal(moltiplicatore) * Decimal(riga.valore_unitario_netto or 0)) - ll["ultimoCosto"])
                 elif riga.id_articolo and not riga.id_listino:
                     lf = leggiFornitura(riga.id_articolo)
-                    totaleRicaricatoLordo += (Decimal(riga.valore_unitario_netto or 0) - lf["prezzoNetto"])
+                    totaleRicaricatoLordo += (Decimal(riga.quantita or 0) * Decimal(moltiplicatore) * Decimal(riga.valore_unitario_netto or 0)) - lf["prezzoNetto"])
             if not riga.moltiplicatore:
                 moltiplicatore = 1
             else:
