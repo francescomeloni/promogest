a15f17ea43a93ccd042033aeb4c3a54acb3f7e34
francesco <francesco@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2011-11-26 14:08:28 +0000

    altro sync con la mia working copy

--- core/PgWeb.py
+++ core/PgWeb.py
@@ -32,7 +32,7 @@ from wsgiref.simple_server import make_server
 
 from promogest import Environment
 from promogest.lib.page import Page
-from promogest.lib.webutils import url_map, jinja_env
+from promogest.lib.webutils import url_map, jinja_env, setconf
 from promogest.lib import routes
 
 class Pg2_web(object):
@@ -48,7 +48,7 @@ class Pg2_web(object):
             print "pazienza"
 
         #TODO: eventuale gestione modulare successiva come nel pg
-        self.importModulesFromDir(modules_dir="core/pages/modules")
+        self.importModulesFromDir(modules_dir="promogest/pages/modules")
         #Aggiungo le directori di lavoro ...
         self.dispatch = SharedDataMiddleware(self.dispatch, {
             '/templates/': Environment.STATIC_PATH,
@@ -56,27 +56,43 @@ class Pg2_web(object):
         })
 
 
-    def importModulesFromDir(self, modules_dir=None):
+    def importModulesFromDir(self, modules_dir):
+        """Check the modules directory and automatically try to load
+        all available modules
+
         """
-        Check the modules directory and automatically try to load all available modules
-        """
-        Environment.modulesList=[]
-        return []
+        #global jinja_env
+        Environment.modulesList=[Environment.tipo_pg]
         modules_folders = [folder for folder in os.listdir(modules_dir) \
                         if (os.path.isdir(os.path.join(modules_dir, folder)) \
                         and os.path.isfile(os.path.join(modules_dir, folder, 'module.py')))]
+        Environment.modules_folders = modules_folders
+
         for m_str in modules_folders:
-            stringa = "%s.%s.module" % (modules_dir.replace("/", "."), m_str)
-            m= __import__(stringa, globals(), locals(), ["m"], -1)
-            Environment.modulesList.append(str(m.MODULES_NAME))
-            for class_name in m.MODULES_FOR_EXPORT:
-                exec 'module = m.'+ class_name
-                self.modules[class_name] = {
-                                    'module': module(),
-                                    'type': module.VIEW_TYPE[0],
-                                    'module_dir': "%s" % (m_str),
-                                    'guiDir':m.GUI_DIR}
-        print "LISTA DEI MODULI CARICATI E FUNZIONANTI", repr(Environment.modulesList)
+            if hasattr(Environment.conf,m_str) or setconf(m_str,"mod_enable", value="yes"):
+                try:
+                    exec "mod_enable = getattr(Environment.conf.%s,'mod_enable')" %m_str
+                except:
+                    mod_enable = setconf(m_str,"mod_enable", value="yes")
+                if mod_enable:
+                    try:
+                        exec "mod_enableyes = getattr(Environment.conf.%s,'mod_enable','yes')" %m_str
+                    except:
+                        mod_enableyes="yes"
+                    if mod_enableyes=="yes":
+                        stringa= "%s.%s.module" % (modules_dir.replace("/", "."), m_str)
+                        m= __import__(stringa, globals(), locals(), ["m"], -1)
+                        Environment.modulesList.append(str(m.MODULES_NAME))
+                        #if hasattr(m,"TEMPLATES"):
+                            #HtmlHandler.templates_dir.append(m.TEMPLATES)
+                        for class_name in m.MODULES_FOR_EXPORT:
+                            exec 'module = m.'+ class_name
+                            self.modules[class_name] = {
+                                                'module': module(),
+                                                'type': module.VIEW_TYPE[0],
+                                                'module_dir': "%s" % (m_str),
+                                                'guiDir':m.GUI_DIR}
+
 
     def dispatch(self, environ, start_response):
         """ Funzione importante per la gestione dello smistamento della request
