1f385be675049e6f39fa863979946b4616f76c57
fmarl <fmarl@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2011-12-16 21:36:06 +0000

    anagrafica banche: fix iban, abi, cab

--- core/promogest/ui/AnagraficaBancheEdit.py
+++ core/promogest/ui/AnagraficaBancheEdit.py
@@ -22,9 +22,10 @@
 #    along with Promogest.  If not, see <http://www.gnu.org/licenses/>.
 
 from promogest.dao.Banca import Banca
-from promogest.lib.iban import country_data, check_iban, IBANError
 from promogest.ui.GladeWidget import GladeWidget
-from promogest.ui.utils import obligatoryField, YesNoDialog
+from promogest.ui.utils import obligatoryField, messageWarning,\
+    messageError
+from promogest.lib.iban import check_iban, IBANError
 
 
 class AnagraficaBancheEdit(GladeWidget):
@@ -47,74 +48,54 @@ class AnagraficaBancheEdit(GladeWidget):
 
     def updateDao(self):
         self.dao = Banca().getRecord(id= self.dao.id)
-#        self._refresh()
 
     def _refresh(self):
-        """ qui ci vanno i dati per riempire le entry e la label"""
         if self.dao and self.dao.denominazione:
             self.denominazione_banca_entry.set_text(str(self.dao.denominazione))
-            self.agenzia_entry.set_text(str(self.dao.agenzia))
-            self.iban_entry.set_text(str(self.dao.iban))
-            self.abi_entry.set_text(str(self.dao.abi))
-            self.cab_entry.set_text(str(self.dao.cab))
+            self.agenzia_entry.set_text(str(self.dao.agenzia or ''))
+            self.iban_entry.set_text(str(self.dao.iban or ''))
+            self.abi_entry.set_text(str(self.dao.abi or ''))
+            self.cab_entry.set_text(str(self.dao.cab or ''))
             self.bic_swift_entry.set_text(str(self.dao.bic_swift or ''))
-            self.checkIban()
 
     def on_calcola_da_iban_button_clicked(self, button):
-        self.checkIban()
+        iban = self.iban_entry.get_text() or ''
+        if iban:
+            try:
+                cc, cs, cin, abi, cab, conto = check_iban(iban.upper())
+            except IBANError as e:
+                messageWarning(msg="Il codice IBAN inserito non è corretto.",
+                               transient=self.getTopLevel())
 
-    def checkIban(self):
-        iban = self.iban_entry.get_text().upper()
-        try:
-            code, checksum, bank, account = check_iban(iban)
-            nazione = country_data(code) or ""
-            cin = bank[:nazione.bank[0][0]] or ""
-            abi = bank[nazione.bank[0][0]:(nazione.bank[0][0]+nazione.bank[1][0])] or ""
-            cab = bank[(nazione.bank[0][0]+nazione.bank[1][0]):(nazione.bank[0][0]+nazione.bank[1][0]+ nazione.bank[2][0])] or ""
-        except IBANError, err:
-            self.info_label.set_markup(str(err))
-            return False
-        else:
-            self.info_label.set_markup("""<b>IBAN CORRETTO!</b>
-
-    NAZIONE: <b>%s</b>  CODE: <b>%s</b> CHECKSUM: <b>%s</b>
-    DATI BANCA: <b>%s</b>  ( CIN: <b>%s</b>, ABI: <b>%s</b>, CAB : <b>%s</b> )
-    N° C/CORRENTE: <b>%s</b>
-
-    Assegna queste coordinate ad un cliente/fornitore
-    o altro contatto se necessario e provvedi al salvataggio.
-
-""" % (nazione.__dict__["name"], code, checksum, bank,cin,abi,cab,account))
-        return iban
 
     def saveDao(self, tipo=None):
         denominazione = self.denominazione_banca_entry.get_text()
-        agenzia = self.agenzia_entry.get_text()
-        iban = self.iban_entry.get_text()
-        abi = self.abi_entry.get_text()
-        cab = self.cab_entry.get_text()
-        bic_swift = self.bic_swift_entry.get_text()
-        if not denominazione :
+        if not denominazione:
             obligatoryField(self.anagrafica.getTopLevel(), self.denominazione_banca_entry)
             return
-#        elif (iban == ''):
-#            obligatoryField(self.anagrafica.getTopLevel(), self.iban_entry)
-#            return
         self.dao.denominazione = denominazione
-        self.dao.agenzia = agenzia
-        self.dao.abi = abi
-        self.dao.cab = cab
-        self.dao.bic_swift = bic_swift
-        ib =  self.checkIban()
-        if ib:
-            self.dao.iban = ib
-            self.dao.persist()
+        self.dao.agenzia = self.agenzia_entry.get_text()
+        self.dao.bic_swift = self.bic_swift_entry.get_text()
+        self.dao.abi = self.abi_entry.get_text()
+        self.dao.cab = self.cab_entry.get_text()
+        
+        iban = self.iban_entry.get_text() or ''
+        if iban:
+            iban = iban.upper()
+            try:
+                cc, cs, cin, abi, cab, conto = check_iban(iban)
+            except IBANError:
+                messageError(msg="Il codice IBAN inserito non è corretto.",
+                                   transient=self.getTopLevel())
+                return
+            else:
+                self.dao.abi = abi
+                self.dao.cab = cab
+                self.dao.iban = iban
         else:
-            msg = """Attenzione! NON e' stato inserito nessun IBAN
-Proseguire comunque?
-"""
-            if YesNoDialog(msg=msg, transient=self.getTopLevel()):
-                self.dao.persist()
+            self.dao.iban = ''
+
+        self.dao.persist()
 
 
     def deleteDao(self):
