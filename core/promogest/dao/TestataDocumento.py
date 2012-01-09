1570b76f4b7d1498a9fb559324e972ba911ccbff
fmarl <fmarl@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2012-01-09 16:07:46 +0000

    documenti: segue il commit precedente

--- core/promogest/dao/TestataDocumento.py
+++ core/promogest/dao/TestataDocumento.py
@@ -313,7 +313,7 @@ class TestataDocumento(Dao):
         if self.id_cliente:
             cliente = leggiCliente(self.id_cliente)
             if not cliente['pagante']:
-                for scad in scadenze:
+                for scad in self.scadenze:
                     if scad:
                         spese += getSpesePagamento(scad.pagamento)
         self._totaleSpese = mN(spese, 2)
