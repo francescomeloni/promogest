7acecc4d9482cf58d6f57e229630ed45aa737c37
fmarl <fmarl@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2012-01-18 09:34:56 +0000

    fix conad

--- core/promogest/tests/test_build_fileconad.py
+++ core/promogest/tests/test_build_fileconad.py
@@ -39,7 +39,8 @@ class TestBuildFileConad(unittest.TestCase):
     """Test per il modulo Trasferimento merce magazzino
     """
     numero = -1
-    print azienda
+    t = None
+    fc = None
 
     def setUp(self):
         testata = TestataDocumento().select(numero="1367")
@@ -47,10 +48,8 @@ class TestBuildFileConad(unittest.TestCase):
             self.t = testata[0]
         self.fc = open("file_conad", "w")
 
-            #print self.t.__dict__
-
     def test_build_fileconad(self):
-        """ Do something interesting here...
+        """ build fileconad
         """
         if self.t:
             #Scriviamo la testata della fattura
@@ -82,13 +81,13 @@ class TestBuildFileConad(unittest.TestCase):
                             art = leggiArticolo(riga.id_articolo)
                             #print riga
                             self.fc.write("02")
-                            self.fc.write(str(dati_differita.index(ddtt)+1).zfill(5))
+                            self.fc.write(str(dati_differita.index(ddtt) + 1).zfill(5))
                             self.fc.write(str(art["codice"]).rjust(15))
-                            self.fc.write(str(art["denominazione"][0:26].replace("à","a")).rjust(30))
+                            self.fc.write(str(art["denominazione"][0:26].replace("à", "a")).rjust(30))
                             self.fc.write(str(art["unitaBase"]).upper().rjust(2))
-                            self.fc.write(str(mN(Decimal(riga.quantita*(riga.moltiplicatore or 1)),2)).replace(".","").zfill(7))
-                            self.fc.write(str(mN(Decimal(riga.valore_unitario_netto),3)).replace(".","").zfill(9))
-                            self.fc.write(str(mN(Decimal(riga.quantita or 0) * Decimal(riga.moltiplicatore or 1) * Decimal(riga.valore_unitario_netto or 0),3)).replace(".","").zfill(9))
+                            self.fc.write(str(mN(Decimal(riga.quantita * (riga.moltiplicatore or 1)), 2)).replace(".","").zfill(7))
+                            self.fc.write(str(mN(Decimal(riga.valore_unitario_netto), 3)).replace(".","").zfill(9))
+                            self.fc.write(str(mN(Decimal(riga.quantita or 0) * Decimal(riga.moltiplicatore or 1) * Decimal(riga.valore_unitario_netto or 0), 3)).replace(".","").zfill(9))
                             self.fc.write(" ".rjust(4))
                             self.fc.write(" ".rjust(1))
                             self.fc.write(str(mN(riga.percentuale_iva,0)).rjust(2))
@@ -105,7 +104,7 @@ class TestBuildFileConad(unittest.TestCase):
                             self.fc.write("".zfill(7))
                             self.fc.write(" ".rjust(3))
                             self.fc.write("".zfill(6))
-                            self.fc.write(" ".rjust(6)+"\n")
+                            self.fc.write(" ".rjust(6) + "\n")
 
             self.fc.close()
 
