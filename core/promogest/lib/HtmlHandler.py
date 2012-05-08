fef912841bc6f1024d5a5a21503a47f9fbc703bb
vete <vete@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2012-05-08 09:32:09 +0000

    fix parsing

--- core/promogest/lib/HtmlHandler.py
+++ core/promogest/lib/HtmlHandler.py
@@ -108,12 +108,15 @@ _paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
 
 @environmentfilter
 def nl2br(eval_ctx, value):
-    result =  '<br />\n'.join(value.split('\n'))
+    if value:
+        result =  '<br />\n'.join(value.split('\n'))
 #    result = u'<br>\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n')
 #                          for p in _paragraph_re.split(escape(value)))
-    if eval_ctx.autoescape:
-        result = Markup(result)
-    return result
+        if eval_ctx.autoescape:
+            result = Markup(result)
+        return result
+    else:
+        return ""
 
 
 """
