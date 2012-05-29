42f133c81bc89dd45c8af856c6e0d0f424548c66
vete <vete@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2012-05-29 09:27:48 +0000

    rimosso pages da qui

--- core/promogest/pages/__init__.py
+++
@@ -1,6 +0,0 @@
-#-*- coding: utf-8 -*-
-#
-# Promogest - Janas
-#
-# Copyright (C) 2005 by Promotux Informatica - http://www.promotux.it/
-# Author: Francesco Meloni <francesco@promotux.it>
--- core/promogest/pages/sitePageList.py
+++
@@ -1,8 +0,0 @@
-#-*- coding: utf-8 -*-
-#
-#PromoCMS
-#
-# Copyright (C) 2005-2008 by Promotux Informatica - http://www.promotux.it/
-# Author: Francesco Meloni <francesco@promotux.it>
-
-pageDict = {"screenshots":"ScreenShots(req=self.req).show()",}
