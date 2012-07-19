f188f4a0cdb467a9d73a9410afd294d76e2f147d
fmarl <fmarl@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2012-07-19 11:20:34 +0000

    dao/fornitura: cleanups

--- core/promogest/dao/Fornitura.py
+++ core/promogest/dao/Fornitura.py
@@ -4,6 +4,8 @@
 #                       di Francesco Meloni snc - http://www.promotux.it/
 
 #    Author: Francesco Meloni  <francesco@promotux.it>
+#    Author: Francesco Marella <francesco.marella@gmail.com>
+
 #    This file is part of Promogest.
 
 #    Promogest is free software: you can redistribute it and/or modify
@@ -23,7 +25,7 @@ from sqlalchemy import *
 from sqlalchemy.orm import *
 from promogest.Environment import *
 from Dao import Dao
-from Multiplo import Multiplo, multiplo
+from Multiplo import Multiplo, t_multiplo
 from ScontoFornitura import ScontoFornitura
 from Fornitore import Fornitore, t_fornitore
 from migrate import *
@@ -34,35 +36,43 @@ class Fornitura(Dao):
     def __init__(self, req=None):
         Dao.__init__(self, entity=self)
 
-    def _getScontiFornitura(self):
-        self.__dbScontiFornitura = self.sconto_fornitura
-        self.__scontiFornitura = self.__dbScontiFornitura[:]
-        return self.__scontiFornitura
+    def sconti():
+        doc = "The sconti property."
+        def fget(self):
+            self.__dbScontiFornitura = self.sconto_fornitura
+            self.__scontiFornitura = self.__dbScontiFornitura[:]
+            return self.__scontiFornitura
+        def fset(self, value):
+            self.__scontiFornitura = value
+        def fdel(self):
+            del self._scontiFornitura
+        return locals()
+    sconti = property(**sconti())
 
-    def _setScontiFornitura(self, value):
-        self.__scontiFornitura = value
+    @property
+    def fornitore(self):
+        if self.forni:
+            return self.forni.ragione_sociale or ""
 
-    sconti = property(_getScontiFornitura, _setScontiFornitura)
+    @property
+    def codice_fornitore(self):
+        if self.forni:
+            return self.forni.codice or ""
 
-    def _fornitore(self):
-        if self.forni: return self.forni.ragione_sociale or ""
-    fornitore= property(_fornitore)
+    @property
+    def codice_articolo(self):
+        if self.arti:
+            return self.arti.codice or ""
 
-    def _codice_fornitore(self):
-        if self.forni: return self.forni.codice or ""
-    codice_fornitore= property(_codice_fornitore)
+    @property
+    def articolo(self):
+        if self.arti:
+            return self.arti.denominazione or ""
 
-    def _codiceArticolo(self):
-        if self.arti: return self.arti.codice or ""
-    codice_articolo= property(_codiceArticolo)
-
-    def _denoArticolo(self):
-        if self.arti: return self.arti.denominazione or ""
-    articolo= property(_denoArticolo)
-
-    def _multiplo(self):
-        if self.multi: return self.multi.denominazione_breve or ""
-    multiplo= property(_multiplo)
+    @property
+    def multiplo(self):
+        if self.multi:
+            return self.multi.denominazione_breve or ""
 
 
     if hasattr(conf, "PromoWear") and getattr(conf.PromoWear,'mod_enable')=="yes":
@@ -91,54 +101,56 @@ class Fornitura(Dao):
             if self.arti:return self.arti.id_anno
         id_anno = property(_id_anno)
 
-        def _denominazione_taglia(self):
-            if self.arti:return self.arti.denominazione_taglia
-        denominazione_taglia = property(_denominazione_taglia)
+        @property
+        def denominazione_taglia(self):
+            if self.arti:
+                return self.arti.denominazione_taglia
 
-        def _denominazione_colore(self):
-            if self.arti:return self.arti.denominazione_colore
-        denominazione_colore = property(_denominazione_colore)
+        @property
+        def denominazione_colore(self):
+            if self.arti:
+                return self.arti.denominazione_colore
 
-        def _anno(self):
-            if self.arti:return self.arti.anno
-        anno = property(_anno)
+        @property
+        def anno(self):
+            if self.arti:
+                return self.arti.anno
 
-        def _stagione(self):
-            if self.arti:return self.arti.stagione
-        stagione = property(_stagione)
+        @property
+        def stagione(self):
+            if self.arti:
+                return self.arti.stagione
 
-        def _genere(self):
+        @property
+        def genere(self):
             if self.arti:return self.arti.genere
-        genere = property(_genere)
-
-
 
     def filter_values(self,k,v):
         if k == 'codiceArticoloFornitore':
-            dic = {k:fornitura.c.codice_articolo_fornitore.ilike("%"+v+"%")}
+            dic = {k: fornitura.c.codice_articolo_fornitore.ilike("%"+v+"%")}
         elif k == 'codiceArticoloFornitoreEM' or k == "codiceArticoloFornitoreEsatto":
-            dic = {k:fornitura.c.codice_articolo_fornitore == v}
-        elif k== 'idFornitore':
-            dic= {k:fornitura.c.id_fornitore ==v}
-        elif k== 'idFornitoreList':
-            dic= {k:fornitura.c.id_fornitore.in_(v)}
+            dic = {k: fornitura.c.codice_articolo_fornitore == v}
+        elif k == 'idFornitore':
+            dic= {k: fornitura.c.id_fornitore ==v}
+        elif k == 'idFornitoreList':
+            dic= {k: fornitura.c.id_fornitore.in_(v)}
         elif k == 'idArticolo':
-            dic = {k:fornitura.c.id_articolo==v}
+            dic = {k: fornitura.c.id_articolo==v}
         elif k == 'idArticoloList':
-            dic = {k:fornitura.c.id_articolo.in_(v)}
+            dic = {k: fornitura.c.id_articolo.in_(v)}
         elif k == 'daDataPrezzo':
-            dic = {k:fornitura.c.data_prezzo >= v}
+            dic = {k: fornitura.c.data_prezzo >= v}
         elif k == 'dataPrezzo':
-            dic = {k:fornitura.c.data_prezzo == v}
+            dic = {k: fornitura.c.data_prezzo == v}
         elif k == 'dataFornitura':
-            dic = {k:fornitura.c.data_fornitura == v}
+            dic = {k: fornitura.c.data_fornitura == v}
         elif k == 'aDataPrezzo':
-            dic = {k:fornitura.c.data_prezzo <= v}
+            dic = {k: fornitura.c.data_prezzo <= v}
         elif k == 'daDataFornitura':
-            dic= {k:fornitura.c.data_fornitura >= v}
+            dic = {k: fornitura.c.data_fornitura >= v}
         elif k == 'aDataFornitura':
-            dic = {k:fornitura.c.data_fornitura <= v}
-        return  dic[k]
+            dic = {k: fornitura.c.data_fornitura <= v}
+        return dic[k]
 
     def scontiFornituraDel(self, idDao):
         aa = ScontoFornitura().select(idFornitura = idDao.id, batchSize=None)
@@ -156,21 +168,23 @@ class Fornitura(Dao):
                 sco.id_fornitura = self.id
                 sco.persist()
 
-fornitura=Table('fornitura', params['metadata'], schema = params['schema'], autoload=True)
+t_fornitura = Table('fornitura',
+                    params['metadata'],
+                    schema=params['schema'],
+                    autoload=True)
 
-if "numero_lotto" not in [c.name for c in fornitura.columns]:
+if "numero_lotto" not in [c.name for c in t_fornitura.columns]:
     col = Column('numero_lotto', String(200))
     col.create(fornitura)
-if "data_scadenza" not in [c.name for c in fornitura.columns]:
+if "data_scadenza" not in [c.name for c in t_fornitura.columns]:
     col = Column('data_scadenza', DateTime)
     col.create(fornitura)
-if "data_produzione" not in [c.name for c in fornitura.columns]:
+if "data_produzione" not in [c.name for c in t_fornitura.columns]:
     col = Column('data_produzione', DateTime)
     col.create(fornitura)
 
-std_mapper = mapper(Fornitura,fornitura, properties={
-        "multi": relation(Multiplo,primaryjoin=fornitura.c.id_multiplo==multiplo.c.id),
+std_mapper = mapper(Fornitura, t_fornitura, properties={
+        "multi": relation(Multiplo, primaryjoin=t_fornitura.c.id_multiplo==t_multiplo.c.id),
         "sconto_fornitura": relation(ScontoFornitura, backref="fornitura"),
-        "forni" : relation(Fornitore,primaryjoin=fornitura.c.id_fornitore==t_fornitore.c.id),
-        #"arti" : relation(Articolo,primaryjoin=fornitura.c.id_articolo==Articolo.id, backref=backref("artic", uselist=False)),
-                }, order_by=[fornitura.c.data_fornitura,fornitura.c.id_fornitore])
+        "forni" : relation(Fornitore, primaryjoin=t_fornitura.c.id_fornitore==t_fornitore.c.id),
+                }, order_by=[t_fornitura.c.data_fornitura, t_fornitura.c.id_fornitore])
