00109fe2636e193bf7d369117252a56a60864dc8
Francesco.Marella@gmail.com <Francesco.Marella@gmail.com@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2012-08-19 17:27:44 +0000

    dao/RicercaLottiUtils: aggiunge header

--- core/promogest/dao/RicercaLottiUtils.py
+++ core/promogest/dao/RicercaLottiUtils.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright (C) 2005-2012 by Promotux
+#                        di Francesco Meloni snc - http://www.promotux.it/
+
+#    Author: Francesco Marella <francesco.marella@gmail.com>
+
+#    This file is part of Promogest.
+
+#    Promogest is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 2 of the License, or
+#    (at your option) any later version.
+
+#    Promogest is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with Promogest.  If not, see <http://www.gnu.org/licenses/>.
+
 from promogest.dao.Fornitura import Fornitura
 from promogest.dao.RigaMovimentoFornitura import RigaMovimentoFornitura
 from promogest.dao.NumeroLottoTemp import NumeroLottoTemp
