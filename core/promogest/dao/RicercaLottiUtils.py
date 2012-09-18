23be20754af1ba5005d58ea5b958fbf1c210905d
Francesco.Marella@gmail.com <Francesco.Marella@gmail.com@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2012-09-18 15:30:22 +0000

    dao/RicercaLottiUtils: correzioni varie

--- core/promogest/dao/RicercaLottiUtils.py
+++ core/promogest/dao/RicercaLottiUtils.py
@@ -37,18 +37,21 @@ def ricerca_lotto(numero_lotto, anno, progress=None):
         aDataFornitura=datetime.datetime.now(),
         batchSize=None)
 
+    docs = []
+
     for fornitura in forniture:
 
         righe_mf = RigaMovimentoFornitura().select(idFornitura=fornitura.id)
-        righe_mov = [riga_mf.rigamovven or riga_mf.rigamovacq for riga_mf in righe_mf]
+
+        righe_mov_acq = [riga_mf.rigamovacq for riga_mf in righe_mf if riga_mf.rigamovacq]
+        righe_mov_ven = [riga_mf.rigamovven for riga_mf in righe_mf if riga_mf.rigamovven]
 
         if progress:
             from promogest.lib.utils import pbar
             pbar(progress, parziale=forniture.index(fornitura), totale=len(forniture),
                 text="Attendere...", noeta=True)
 
-        docs = []
-        for riga_mov in righe_mov:
+        for riga_mov in righe_mov_acq:
             if not riga_mov:
                 continue
             tm = TestataMovimento().getRecord(id=riga_mov.id_testata_movimento)
@@ -56,8 +59,16 @@ def ricerca_lotto(numero_lotto, anno, progress=None):
                 if tm.TD and tm.TD not in docs:
                     docs.append(tm.TD)
 
-            docs.extend(ricerca_in_lottotemp(numero_lotto))
-            
+        for riga_mov in righe_mov_ven:
+            if not riga_mov:
+                continue
+            tm = TestataMovimento().getRecord(id=riga_mov.id_testata_movimento)
+            if tm:
+                if tm.TD and tm.TD not in docs:
+                    docs.append(tm.TD)
+
+    docs.extend(ricerca_in_lottotemp(numero_lotto))
+
     lista_fornitori.append({'data_fornitura': fornitura.data_fornitura,
                 'fornitore': fornitura.forni, 'docs': docs})
     return lista_fornitori
