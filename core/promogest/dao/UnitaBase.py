ddf575d7e5b29d4a695f6e0202c63c42b526c754
m3nt0r3@gmail.com <m3nt0r3@gmail.com@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2013-01-17 13:05:40 +0000

    aggiunte altre unità di misura oltre quelle standard

--- core/promogest/dao/UnitaBase.py
+++ core/promogest/dao/UnitaBase.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
 
-# Promogest
-# Copyright (C) 2005 by Promotux Informatica - http://www.promotux.it/
-# Author: Francesco Meloni <francesco@promotux.it>
+#    Copyright (C) 2005-2013 by Promotux
+#                        di Francesco Meloni snc - http://www.promotux.it/
+
+#    Author: Francesco Meloni  <francesco@promotux.it>
+#    Author: Francesco Marella <francesco.marella@anche.no>
+
+#    This file is part of Promogest.
+
+#    Promogest is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 2 of the License, or
+#    (at your option) any later version.
+
+#    Promogest is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with Promogest.  If not, see <http://www.gnu.org/licenses/>.
 
 from sqlalchemy import *
 from sqlalchemy.orm import *
@@ -19,31 +36,50 @@ class UnitaBase(Dao):
         dic= {'denominazione' : unitabase.c.denominazione ==v}
         return  dic[k]
 
-    def addMC(self):
-        mc = self.select(denominazione ="Metri Cubi")
-        if not mc:
-            self.denominazione_breve = "mc"
-            self.denominazione = "Metri Cubi"
-            self.persist()
-        gr = self.select(denominazione ="Grammi")
-        if not gr:
-            self.denominazione_breve = "gr"
-            self.denominazione = "Grammi"
-            self.persist()
-        ml = self.select(denominazione ="Millilitri")
-        if not ml:
-            self.denominazione_breve = "ml"
-            self.denominazione = "Millilitri"
-            self.persist()
-        q = self.select(denominazione ="Quintali")
-        if not q:
-            self.denominazione_breve = "q"
-            self.denominazione = "Quintali"
-            self.persist()
+    #def addMC(self):
+        #mc = self.select(denominazione ="Metri Cubi")
+        #if not mc:
+            #self.denominazione_breve = "mc"
+            #self.denominazione = "Metri Cubi"
+            #self.persist()
+        #gr = self.select(denominazione ="Grammi")
+        #if not gr:
+            #self.denominazione_breve = "gr"
+            #self.denominazione = "Grammi"
+            #self.persist()
+        #ml = self.select(denominazione ="Millilitri")
+        #if not ml:
+            #self.denominazione_breve = "ml"
+            #self.denominazione = "Millilitri"
+            #self.persist()
+        #q = self.select(denominazione ="Quintali")
+        #if not q:
+            #self.denominazione_breve = "q"
+            #self.denominazione = "Quintali"
+            #self.persist()
 
 unitabase=Table('unita_base',
                 params['metadata'],
                 schema = params['mainSchema'],
                 autoload=True)
+
+s= select([unitabase.c.denominazione_breve]).execute().fetchall()
+if (u'mc',) not in s or s==[]:
+    o = unitabase.insert()
+    o.execute(denominazione = "Metri Cubi",denominazione_breve="mc")
+if (u'gr',) not in s or s==[]:
+    o = unitabase.insert()
+    o.execute(denominazione = "Grammi",denominazione_breve="gr")
+if (u'ml',) not in s or s==[]:
+    o = unitabase.insert()
+    o.execute(denominazione = "Millilitri",denominazione_breve="ml")
+if (u'q',) not in s or s==[]:
+    o = unitabase.insert()
+    o.execute(denominazione = "Quintali",denominazione_breve="q")
+if (u't',) not in s or s==[]:
+    o = unitabase.insert()
+    o.execute(denominazione = "Tonnellate",denominazione_breve="t")
+
+
 
 std_mapper = mapper(UnitaBase,unitabase)
