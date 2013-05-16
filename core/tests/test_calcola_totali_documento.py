95754fe376c1defec603d70bae162b7a2659a7ed
porcupineride@gmail.com <porcupineride@gmail.com@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2013-05-16 22:23:29 +0000

    test: test per il controllo del totale documento

--- core/tests/test_calcola_totali_documento.py
+++ core/tests/test_calcola_totali_documento.py
@@ -21,7 +21,6 @@
 
 import unittest
 import sys
-import os
 path = ".."
 if path not in sys.path:
     sys.path.append(path)
@@ -31,46 +30,79 @@ preEnv.tipodbforce = "postgresql"
 preEnv.aziendaforce = "urbani"
 from promogest.buildEnv import set_configuration
 from promogest import Environment
-conf = set_configuration("urbani", "2012")
+conf = set_configuration("urbani", "2013")
 Environment.conf = conf
-print conf
-from multiprocessing import Pool
+from promogest.Environment import session
 
 from promogest.dao.TestataDocumento import TestataDocumento
-daos = TestataDocumento().select(batchSize=1000)
+from decimal import Decimal
+
 
 class TestTestataDocumentoTotali(unittest.TestCase):
 
-    def setUp(self):
-        self.p = Pool(4)
-        #self.daos = TestataDocumento().select(batchSize=1)
+    def test_doc1(self):
+        """controllo totali per il documento 23701"""
+        doc = session.query(TestataDocumento).filter_by(operazione="Fattura differita vendita", id=23701).one()
+        doc.totali
+        assert doc._totaleImponibile == Decimal('88.52')
+        assert doc._totaleImposta == Decimal('8.85')
+        for info_iva in doc._castellettoIva:
+            if info_iva['aliquota'] == Decimal(10):
+                assert info_iva['imponibile'] == Decimal("88.52")
+                assert info_iva['imposta'] == Decimal("8.85")
+                assert info_iva['totale'] == Decimal("97.37")
 
-    def tota(self, d):
-        d.totali
+    def test_doc2(self):
+        """controllo totali per il documento 23700"""
+        doc = session.query(TestataDocumento).filter_by(operazione="Fattura differita vendita", id=23700).one()
+        doc.totali
+        assert doc._totaleImponibile == Decimal('224.04')
+        assert doc._totaleImposta == Decimal('22.40')
+        for info_iva in doc._castellettoIva:
+            if info_iva['aliquota'] == Decimal(10):
+                assert info_iva['imponibile'] == Decimal("224.04")
+                assert info_iva['imposta'] == Decimal("22.40")
+#                assert info_iva['totale'] == Decimal("97.37")
 
-    def test_totali(self):
-        self.p.imap(totali, daos)
-        #results = self.p.map(d.totali,daos) for d in daos
-        self.p.terminate()
-        #print 1
-        # make sure the shuffled sequence does not lose any elements
-        for d in daos:
-            #d.totali
-            print d.id ,  len(d.righe), d._totaleImponibileScontato
-            #self.assertEqual(d.id, 10)
+    def test_doc3(self):
+        """controllo totali per il documento 23699"""
+        doc = session.query(TestataDocumento).filter_by(operazione="Fattura differita vendita", id=23699).one()
+        doc.totali
+        assert doc._totaleImponibile == Decimal('90.21')
+        assert doc._totaleImposta == Decimal('8.34')
+        for info_iva in doc._castellettoIva:
+            if info_iva['aliquota'] == Decimal(4):
+                assert info_iva['imponibile'] == Decimal("11.4")
+                assert info_iva['imposta'] == Decimal("0.46")
+                assert info_iva['totale'] == Decimal("11.86")
+            if info_iva['aliquota'] == Decimal(10):
+                assert info_iva['imponibile'] == Decimal("78.81")
+                assert info_iva['imposta'] == Decimal("7.88")
+                assert info_iva['totale'] == Decimal("86.69")
 
-        # should raise an exception for an immutable sequence
-        #self.assertRaises(TypeError, random.shuffle, (1,2,3))
+    def test_doc4(self):
+        """controllo totali per il documento 23697"""
+        doc = session.query(TestataDocumento).filter_by(operazione="Fattura differita vendita", id=23697).one()
+        doc.totali
+        assert doc._totaleImponibile == Decimal('63.44')
+        assert doc._totaleImposta == Decimal('2.54')
+        for info_iva in doc._castellettoIva:
+            if info_iva['aliquota'] == Decimal(4):
+                assert info_iva['imponibile'] == Decimal("63.44")
+                assert info_iva['imposta'] == Decimal("2.54")
+                assert info_iva['totale'] == Decimal("65.98")
 
-    #def test_choice(self):
-        #element = random.choice(self.seq)
-        #self.assertTrue(element in self.seq)
-
-    #def test_sample(self):
-        #with self.assertRaises(ValueError):
-            #random.sample(self.seq, 20)
-        #for element in random.sample(self.seq, 5):
-            #self.assertTrue(element in self.seq)
+    def test_doc5(self):
+        """controllo totali per il documento 23694"""
+        doc = session.query(TestataDocumento).filter_by(operazione="Fattura differita vendita", id=23694).one()
+        doc.totali
+        assert doc._totaleImponibile == Decimal('79.86')
+        assert doc._totaleImposta == Decimal('7.99')
+        for info_iva in doc._castellettoIva:
+            if info_iva['aliquota'] == Decimal(10):
+                assert info_iva['imponibile'] == Decimal("79.86")
+                assert info_iva['imposta'] == Decimal("7.99")
+                assert info_iva['totale'] == Decimal("87.85")
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestTestataDocumentoTotali)
 unittest.TextTestRunner(verbosity=3).run(suite)
