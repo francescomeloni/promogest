e32a87e154a6e142ed3688201605f59f4ce26276
m3nt0r3@gmail.com <m3nt0r3@gmail.com@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2013-08-12 13:05:54 +0000

    dimenticato

---
+++ core/data/faq.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright (C) 2005-2013 by Promotux
+#                        di Francesco Meloni snc - http://www.promotux.it/
+
+#    Author: Francesco Meloni  <francesco@promotux.it>
+
+#    This file is part of Promogest.
+
+#    Promogest is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 2 of the License, or
+#    (at your option) any later version.
+
+#    Promogest is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with Promogest.  If not, see <http://www.gnu.org/licenses/>.
+
+from sqlalchemy import *
+from promogest.Environment import *
+
+
+t_faq= Table('faq', params['metadata'],
+        Column('id', Integer, primary_key=True),
+        Column('question', String(200), nullable=False),
+        Column('argomento', String(200), nullable=True),
+        Column('tags', String(600), nullable=True),
+        Column('answer', Text, nullable=True),
+        Column('imagepath', String(400), nullable=True),
+        Column('insert_date', DateTime, nullable=True),
+        Column('clicks', Integer),
+        Column("permalink", String(500), nullable=True),
+        Column('active', Boolean, default=0),
+        schema=params['schema']
+        )
+t_faq.create(checkfirst=True)
