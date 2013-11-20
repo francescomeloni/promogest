819777de5afd33b07e233f2bd0e8840a5a154a6d
m3nt0r3@gmail.com <m3nt0r3@gmail.com@c8087da8-80d7-f218-d0bb-7d131f7cdc9e>
2013-11-20 14:30:15 +0000

    fix codice fornitore

--- core/promogest/dao/Fornitore.py
+++ core/promogest/dao/Fornitore.py
@@ -19,8 +19,8 @@
 #    You should have received a copy of the GNU General Public License
 #    along with Promogest.  If not, see <http://www.gnu.org/licenses/>.
 
-from sqlalchemy import Table, or_
-from sqlalchemy.orm import mapper, relation, join
+from sqlalchemy import *
+from sqlalchemy.orm import *
 from promogest.Environment import params, conf, session
 
 try:
